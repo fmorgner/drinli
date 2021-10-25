67a0f59f0ad9c880a174a615c90a7d8ef1767a2a
Felix Morgner <felix.morgner@gmail.com>
2021-10-25 23:22:35 +0200

    build: adjust openocd config path

--- source/cmake/Modules/AddFlashTarget.cmake
+++ source/cmake/Modules/AddFlashTarget.cmake
@@ -2,7 +2,7 @@ function(add_flash_target TARGET)
     message(STATUS "Creating flash target (flash_${TARGET}) for '${TARGET}'")
 
     add_custom_target("flash_${TARGET}"
-        COMMAND "openocd" "-f" "${PROJECT_SOURCE_DIR}/openocd.cfg" "-c" "program $<TARGET_FILE:${TARGET}> verify reset exit"
+        COMMAND "openocd" "-f" "${PROJECT_SOURCE_DIR}/../openocd.cfg" "-c" "program $<TARGET_FILE:${TARGET}> verify reset exit"
         DEPENDS "app"
         COMMENT "Programming device ..."
     )
