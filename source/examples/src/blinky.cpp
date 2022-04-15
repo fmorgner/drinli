e42639894e5c0c154342307317394629ba02ead2
Felix Morgner <felix.morgner@gmail.com>
2022-04-16 01:49:38 +0200

    rom: reintroduce typed GPIO functions

--- source/examples/src/blinky.cpp
+++ source/examples/src/blinky.cpp
@@ -1,5 +1,12 @@
 #include <platform/firmware.hpp>
+#include <rom/gpio.hpp>
 #include <rom/system_control.hpp>
+
+auto constexpr red_led = 0b0000'0010;
+auto constexpr blue_led = 0b0000'0100;
+auto constexpr green_led = 0b0000'1000;
+
+auto constexpr all_leds = red_led | blue_led | green_led;
 
 [[noreturn]] auto firmware_main() -> void
 {
@@ -7,6 +14,14 @@
 
   rom::system_control::enable(rom::system_control::gpio::f);
   rom::system_control::enable_ahb(rom::system_control::gpio::f);
+
+  rom::gpio::set_mode(0x4005'D000, all_leds, rom::gpio::pin_mode::output);
+  rom::gpio::set_pad_configuration(0x4005'D000,
+                                   all_leds,
+                                   rom::gpio::drive_strength::basic_2mA,
+                                   rom::gpio::pin_type::push_pull_with_weak_pull_down);
+
+  rom::gpio::write(0x4005'D000, all_leds, true);
 
   while (true)
   {
--- source/rom/include/rom/gpio.hpp
+++ source/rom/include/rom/gpio.hpp
@@ -4,13 +4,43 @@
 #include "rom/api.hpp"
 
 #include <cstdint>
-#include <initializer_list>
-#include <iterator>
-#include <numeric>
-#include <type_traits>
 
 namespace drinli::rom::gpio
 {
+
+  namespace detail
+  {
+    auto inline static untyped_write(std::uint32_t port, std::uint8_t pins, std::uint8_t value) -> void
+    {
+      return api::tables::gpio::function<0, decltype(untyped_write)>::invoke(port, pins, value);
+    }
+  }  // namespace detail
+
+  enum struct drive_strength : std::uint32_t
+  {
+    basic_2mA = 0x0000'0001,
+    basic_4mA = 0x0000'0002,
+    basic_8mA = 0x0000'0004,
+    slew_rate_controlled_8mA = 0x0000'000C,
+  };
+
+  enum struct pin_mode : std::uint32_t
+  {
+    input = 0x0000'0000,
+    output = 0x0000'0001,
+    peripheral = 0x0000'0002,
+  };
+
+  enum struct pin_type : std::uint32_t
+  {
+    analog_comparator = 0x0000'0000,
+    push_pull = 0x0000'0008,
+    push_pull_with_weak_pull_up = 0x0000'000A,
+    push_pull_with_weak_pull_down = 0x0000'000C,
+    open_drain = 0x0000'0009,
+    open_drain_with_weak_pull_up = 0x0000'000B,
+    open_drain_with_weak_pull_down = 0x0000'000D,
+  };
 
   /**
    * @brief Set the mode of the given pins in the give port
@@ -19,20 +49,20 @@ namespace drinli::rom::gpio
    * @param pins A set of pins on the `port`
    * @param mode The target mode of the `pins` on `port`
    */
-  auto inline static set_mode(std::uint32_t port, std::uint8_t pins, std::uint32_t mode) -> void
+  auto inline static set_mode(std::uint32_t port, std::uint8_t pins, pin_mode mode) -> void
   {
     return api::tables::gpio::function<1, decltype(set_mode)>::invoke(port, pins, mode);
   }
 
   /**
-   * @brief Set the configuration of the desired GPIO pad
+   * @brief Set the configuration of the desired GPIO pad.
    *
    * @param port A GPIO port
    * @param pins A set of pins on the `port`
    * @param strength The desired drive strength
    * @param type The desired drive type
    */
-  auto inline static set_pad_configuration(std::uint32_t port, std::uint8_t pins, std::uint32_t strength, std::uint32_t type)
+  auto inline static set_pad_configuration(std::uint32_t port, std::uint8_t pins, drive_strength strength, pin_type type)
       -> void
   {
     return api::tables::gpio::function<5, decltype(set_pad_configuration)>::invoke(port, pins, strength, type);
@@ -56,9 +86,9 @@ namespace drinli::rom::gpio
    * @param pins A set of pins on the `port`
    * @param value The value to apply to the pins
    */
-  auto inline static write(std::uint32_t port, std::uint8_t pins, std::uint8_t value) -> void
+  [[gnu::noinline]] auto inline static write(std::uint32_t port, std::uint8_t pins, bool value) -> void
   {
-    return api::tables::gpio::function<0, decltype(write)>::invoke(port, pins, value);
+    return detail::untyped_write(port, pins, (value ? 0xff : 0x00) & pins);
   }
 
 }  // namespace drinli::rom::gpio
