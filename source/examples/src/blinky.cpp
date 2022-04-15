92a7db739c7ae3c14ee4677f76feac5d7605e5f7
Felix Morgner <felix.morgner@gmail.com>
2022-04-15 13:41:07 +0200

    examples: remove inactive code

--- source/examples/src/blinky.cpp
+++ source/examples/src/blinky.cpp
@@ -1,31 +1,5 @@
-// #include <rom/gpio.hpp>
-// #include <platform/gpio.hpp>
 #include <platform/firmware.hpp>
 #include <rom/system_control.hpp>
-
-// using gpio = drinli::rom::gpio;
-// using sysctl = drinli::rom::system_control;
-
-// auto constexpr red_led = gpio::pin::_1;
-// auto constexpr blue_led = gpio::pin::_2;
-// auto constexpr green_led = gpio::pin::_3;
-
-// auto constexpr all_leds = gpio::pin_set{
-//     red_led,
-//     blue_led,
-//     green_led,
-// };
-
-// auto firmware_main() -> void
-// {
-//   sysctl::enable(sysctl::peripheral::gpio_f);
-//   sysctl::enable_ahb(sysctl::peripheral::gpio_f);
-
-//   gpio::set_mode(gpio::port::f, all_leds, gpio::mode::out);
-//   gpio::set_pad_configuration(gpio::port::f, all_leds, gpio::strength::_2mA, gpio::type::push_pull_with_weak_pull_down);
-
-//   gpio::write(gpio::port::f, all_leds, gpio::level::high);
-// }
 
 [[noreturn]] auto firmware_main() -> void
 {
